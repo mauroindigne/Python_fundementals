8b02e26efa6d22db85cf47df2a26a8d68edd3853
Mauro Indigne <mauroindigne@gmail.com>
2021-03-16 19:40:14 +0100

    first 03 push

--- 03_more_datatypes/2_lists/03_07_duplicates.py
+++ 03_more_datatypes/2_lists/03_07_duplicates.py
@@ -5,3 +5,9 @@ Write a script that removes all duplicates from a list.
 '''
 
 list_ = [1, 2, 3, 4, 3, 4, 5]
+
+clean = []
+for i in list_:
+    if i not in clean:
+        clean.append(i)
+print(clean)
