9fd70da53994744294723aade1d690ba9c5c1b39
Mauro Indigne <mauroindigne@gmail.com>
2021-04-27 18:21:40 +0700

    args and kwargs

--- 16_variable_arguments/16_01_args.py
+++ 16_variable_arguments/16_01_args.py
@@ -2,3 +2,10 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+
+def my_function(*args):
+    for i in args:
+        print(f"{i.capitalize()}s")
+
+my_function('bird', 'dog', "cow", 'horse')
+
--- 16_variable_arguments/16_02_kwargs.py
+++ 16_variable_arguments/16_02_kwargs.py
@@ -2,3 +2,15 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+
+names = []
+
+def my_function(**kwargs):
+    for k, v in kwargs.items():
+        names.append(v)
+        print(f'{k}: {v.capitalize()}')
+
+
+my_function(name1='john', name2='steven', name3='bob', name4='jerry')
+
+print(f'list if names: {names}')
