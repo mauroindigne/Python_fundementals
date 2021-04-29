5e1c1a935dd012229201960774a996b08ec9c69d
Mauro Indigne <mauroindigne@gmail.com>
2021-04-29 17:35:02 +0700

    finished decorators

--- 17_decorators/17_01_wraps.py
+++ 17_decorators/17_01_wraps.py
@@ -1,3 +1,12 @@
 """
 Write a decorator function that wraps text passed to it in a html <p> tag.
 """
+
+def my_func(msg):
+    def inner_func():
+        print("</p>"+msg+"</p>")
+    return inner_func
+
+message = "Now we have an argument!"
+woohoo = my_func(message)
+woohoo()
--- 17_decorators/17_02_tags.py
+++ 17_decorators/17_02_tags.py
@@ -3,3 +3,19 @@ Improve the decorator from the previous exercise by allowing it to take
 any specified HTML tag as an input - making it more general.
 
 '''
+
+def my_func(start_tag, msg, end_tag):
+    def inner_func():
+        print(start_tag+msg+end_tag)
+    return inner_func
+
+html_tag = input("Which html tag do you want?:")
+
+message = "Now we have an argument!"
+html_start_tag = "<"+html_tag+">"
+html_end_tag = "</"+html_tag+">"
+html_code = my_func(html_start_tag, message, html_end_tag)
+
+
+
+html_code()
